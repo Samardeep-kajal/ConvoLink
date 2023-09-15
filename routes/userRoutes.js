34c299db48d0801da1c1a405984b13193161eee1
sarthak <sarthakgupta878@gmail.com>
2023-09-15 17:38:02 +0530

    added backend of user-login TO BE VERIFIED THROUGH MONGODB ATLAS  On branch sarthak/login_signup  Changes to be committed: 	modified:   middleware/errorMiddleware.js 	modified:   models/userModel.js 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/userRoutes.js 	modified:   server.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userModel')
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/errorMiddleware');
+
+const JWT_SECRET = 'kskalnadssalsbjk';
+
+router.post('/createuser', [
+    body('name', 'Enter a valid Name').isLength({ min: 3 }),
+    body('password', 'Password must be 5 characters long').isLength({ min: 5 }),
+    body('email', 'Enter a valid Email').isEmail(),
+], async (req, res) => {
+    let success=false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success,errors: errors.array() });
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+       const secPass = await bcrypt.hash(req.body.password,salt)
+        let user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        });
+        const data = {
+            user:{
+                id: user._id
+            }
+        }
+      const authToken =  jwt.sign(data, JWT_SECRET);
+    
+        success = true;
+        res.json({success,authToken})
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+router.post('/login', [
+   
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'password cannot be blank').exists(),
+
+], async (req, res) => {
+    let success=false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            success=false;
+            return res.status(400).json({success, error:"try to login with correct Credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password)
+        if(!passwordCompare){
+            success=false;
+            return res.status(400).json({success, error:"try to login with correct Credentials"});
+        }
+
+        const data = {
+            user:{
+                id:user._id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        success=true;
+        res.json({success,authToken})
+    }
+    catch(err){
+        res.status(500).json({ error: "Internal Server Error" })
+
+    }
+})
+router.post('/getuser', fetchuser,async (req, res) => {
+
+try{
+    let userId= req.user.id;
+    const user = await User.findById(userId).select("-password")
+    res.send(user)
+}
+catch(err){
+    res.status(500).send("Internal Server Error ")
+}
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectDB = require("./config/db");
 const ejsMate = require('ejs-mate')
+var cors = require('cors')
 const app = express();
 
 connectDB();
@@ -10,11 +11,13 @@ app.engine('ejs', ejsMate)
 app.set('views engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+app.use('/api/auth',require('./routes/auth'))
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
-
 
 const port = 8000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
