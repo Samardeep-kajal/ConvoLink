9bcc0f031c21907bb6875fea007d572245786327
Samardeep Kajal <samk712003@gmail.com>
2023-10-23 13:21:11 +0530

    Fixed Chat section

--- server.js
+++ server.js
@@ -53,6 +53,9 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
     socket.on("disconnect", () => {
       socket.broadcast.to(roomId).emit("user-disconnected", userId);
     });
